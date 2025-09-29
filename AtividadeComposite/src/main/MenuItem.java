09fc61a5f6e525379d5e330b45bc035a41e5b0e4
Hernandes S. Pereira <hpereira23@outlook.com>
2025-09-29 19:17:08 -0300

    add Atividade Composite

---
+++ AtividadeComposite/src/main/MenuItem.java
@@ -0,0 +1,15 @@
+package main;
+
+public class MenuItem implements MenuComponent{
+	
+		private String name;
+		public MenuItem(String name) {
+		this.name = name;
+		}
+	
+	@Override
+	public void show() {
+		System.out.println("[] " + name);
+	}
+
+}
